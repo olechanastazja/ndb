51d3128ae85576f2a93780f4f28e94423c5b8ee6
olechanastazja <agnieszka.anastazja.olech@gmail.com>
2020-03-15 14:49:45 +0100

    Fifth and sixth exercise

--- src/Collections.scala
+++ src/Collections.scala
@@ -1,11 +1,24 @@
-object ProductsMap {
+object Collections {
 
   def main(args: Array[String]) {
+    println("New map after discount: " + createProductMaps())
+    val myTuple = (6, "ABC", 2.0)
+    println("Diverse tuple: " + printDiverseTuple(myTuple))
+  }
+
+  // zadanie 5
+  def createProductMaps() : Map[String, Double] = {
     val mapProducts = Map("Chocolate" -> 20,
       "Ice cream" -> 30,
       "Apple" -> 10)
     val afterDiscount = mapProducts.map(kv => (kv._1, kv._2*0.9))
-    println("New map after discount: " + afterDiscount)
+    afterDiscount
+  }
+  // zadanie 6
+  def printDiverseTuple(myTuple: (Int, String, Double)) : Unit = {
+    myTuple.productIterator.foreach(x => {
+      println(x)
+    })
   }
 
 }
