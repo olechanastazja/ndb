b9234079bd1204f705b55bfc6ade1019e8c312d3
olechanastazja <agnieszka.anastazja.olech@gmail.com>
2020-03-22 19:31:09 +0100

    Add content

--- src/nbd_2/PatternMatching.scala
+++ src/nbd_2/PatternMatching.scala
@@ -1,5 +1,18 @@
 package nbd_2
 
-object PatternMatching {
 
+object PatternMatching {
+  val work_days = List("Poniedziałek", "Wtorek", "Środa", "Czwartek", "Piątek")
+  val weekend_days = List("Sobota", "Niedziela")
+
+  def main(args: Array[String]) {
+    println(isItWeekend("Sobota"))
+  }
+
+  // zadanie 1
+  def isItWeekend(day: String): String  = day match {
+    case day if work_days.contains(day) => "Praca"
+    case day if weekend_days.contains(day) => "Weekend"
+    case day => "Nie ma takiego dnia"
+  }
 }
